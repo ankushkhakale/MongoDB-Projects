ade8f8ce77a8150aea6acd1f8a38257f8d81c0a3
ankushkhakale <ankushkhakale@gmail.com>
2026-05-07 23:26:47 +0530

    deleted replicas

--- Study-tracker/backend/db.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-
-# 👉 Use MongoDB Atlas URI later if needed
-client = MongoClient("mongodb://localhost:27017/")
-
-db = client["study_tracker"]
-collection = db["sessions"]
